adb7a4495acdb9a353b7b2a1612988048a5ba263
renu629 <renu.chirumamilla@gmail.com>
2025-07-20 22:55:48 -0500

    add configs to workspace

---
+++ UsernameService/Migrations/20250720033706_Init.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UsernameService.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UsernameRecords",
+                columns: table => new
+                {
+                    AccountId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Username = table.Column<string>(type: "TEXT", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsernameRecords", x => x.AccountId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsernameRecords_Username",
+                table: "UsernameRecords",
+                column: "Username",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UsernameRecords");
+        }
+    }
+}
---
+++ UsernameService/Models/UsernameRecord.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsernameService.Models;
+
+public class UsernameRecord
+{
+    [Key]
+    public Guid AccountId { get; set; }
+
+    [Required]
+    [StringLength(30, MinimumLength = 6)]
+    [RegularExpression("^[a-zA-Z0-9]*$", ErrorMessage = "Username must be alphanumeric only.")]
+    public string Username { get; set; } = string.Empty;
+}
